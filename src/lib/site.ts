138ed589cf8a0c56dd06e703121e35261b82fe88
Gonaaal <87767165+Gonaaal@users.noreply.github.com>
2026-09-11 17:49:23 +0200

    Add site configuration constants and asset imports

---
+++ src/lib/site.ts
@@ -0,0 +1,16 @@
+import gal1 from "@/assets/gal1.png.asset.json";
+import gal2 from "@/assets/gal2.png.asset.json";
+import normativaGeneral from "@/assets/normativa-general.pdf.asset.json";
+import normativaDiscord from "@/assets/normativa-discord.pdf.asset.json";
+
+export const DISCORD_URL = "https://discord.gg/valenciapd";
+export const SERVER_IP = "connect cfx.re/join/qqqb8mz";
+export const SERVER_CONNECT = "https://cfx.re/join/qqqb8mz";
+export const TIKTOK_URL = "https://www.tiktok.com/@valenciapd_";
+export const INSTAGRAM_URL = "http://instagram.com/valenciapd_/";
+export const TWITCH_URL = "https://www.twitch.tv/valenciapd";
+
+export const NORMATIVA_GENERAL_URL = normativaGeneral.url;
+export const NORMATIVA_DISCORD_URL = normativaDiscord.url;
+
+export const GALLERY = [gal1.url, gal2.url] as const;
